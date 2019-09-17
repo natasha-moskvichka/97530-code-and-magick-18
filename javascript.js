0b759df9c309c59172d07af5c92407ebf048de64
Natalia <y_natalya_v@mail.ru>
2019-09-17 13:08:32 +0300

    add .js
    module1-task2

---
+++ javascript.js
@@ -0,0 +1,24 @@
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getFireballSpeed = function(left) {
+  if (left === true) {
+    return 5;
+  }
+    return 2;
+};
+
+var getWizardHeight = function() {
+  return 1.337 * wizardWidth;
+};
+
+var getWizardX = function(width) {
+  return width / 2;
+}
+
+var getWizardY = function(height) {
+  return height * 0.6;
+}
+
+
